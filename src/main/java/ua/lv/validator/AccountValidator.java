60e1d4dc94c0e3b80c12ce8cc8d38d2e92ffee1f
derzhylo <yderzhylo@gmail.com>
2017-12-16 22:35:00 +0200

    fixed

--- src/main/java/ua/lv/validator/AccountValidator.java
+++ src/main/java/ua/lv/validator/AccountValidator.java
@@ -1,46 +1,46 @@
-package ua.lv.validator;
-
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cglib.core.Local;
-import org.springframework.stereotype.Component;
-import org.springframework.validation.Errors;
-import org.springframework.validation.Validator;
-import ua.lv.entity.Account;
-import ua.lv.service.AccountService;
-
-import org.joda.time.LocalDate;
-import java.util.Date;
-
-
-@Component
-public class AccountValidator implements Validator {
-
-
-    @Autowired
-    AccountService accountService;
-
-
-
-
-    public boolean supports(Class<?> aClass) {
-        return aClass.equals(Account.class);
-    }
-
-    public void validate(Object o, Errors errors) {
-        Account account = (Account) o;
-
-        if ((account.getGoalCrNum() < 3) || (account.getGoalCrNum() > 32)) {
-            errors.rejectValue("goalCrNum", "error", "ups.");
-        }
-
-       LocalDate  dateNow = LocalDate.now();
-       LocalDate deadline = LocalDate.parse(account.getDeadline());
-       boolean d = dateNow.isBefore(deadline);
-       if(d){
-           errors.rejectValue("date", "errors", "your date false");
-       }
+//package ua.lv.validator;
+//
+//import org.joda.time.format.DateTimeFormat;
+//import org.joda.time.format.DateTimeFormatter;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.cglib.core.Local;
+//import org.springframework.stereotype.Component;
+//import org.springframework.validation.Errors;
+//import org.springframework.validation.Validator;
+//import ua.lv.entity.Account;
+//import ua.lv.service.AccountService;
+//
+//import org.joda.time.LocalDate;
+//import java.util.Date;
+//
+//
+//@Component
+//public class AccountValidator implements Validator {
+//
+//
+//    @Autowired
+//    AccountService accountService;
+//
+//
+//
+//
+//    public boolean supports(Class<?> aClass) {
+//        return aClass.equals(Account.class);
+//    }
+//
+//    public void validate(Object o, Errors errors) {
+//        Account account = (Account) o;
+//
+//        if ((account.getGoalCrNum() < 3) || (account.getGoalCrNum() > 32)) {
+//            errors.rejectValue("goalCrNum", "error", "ups.");
+//        }
+//
+//       LocalDate  dateNow = LocalDate.now();
+//       LocalDate deadline = LocalDate.parse(account.getDeadline());
+//       boolean d = dateNow.isBefore(deadline);
+//       if(d){
+//           errors.rejectValue("date", "errors", "your date false");
+//       }
 //        Date deadLine = new Date(account.getDeadline());
 //        System.out.println(deadLine);
 //        Date dateNow = new Date();
@@ -50,5 +50,5 @@ public class AccountValidator implements Validator {
 //            errors.rejectValue("date", "errors", "your date false");
 //        }
 
-    }
-}
+//    }
+//}
