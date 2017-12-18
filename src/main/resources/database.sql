649b12aee4b80fa798e868d3d0260e3ee837ea62
Bogdan Vavruk <bogdan.vav@gmail.com>
2017-12-18 14:03:24 +0200

    apdate admin

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,5 +1,5 @@
 
 
-INSERT INTO user VALUES (2,1,1,'ROLE_ADMIN',NULL ,1,'loginoleg123@gmail.com',
-                            1,'Oleg', 'Login','$2a$11$uSXS6rLJ91WjgOHhEGDx..VGs7MkKZV68Lv5r1uwFu7HgtRn3dcXG','Travolta');
+INSERT INTO user VALUES (4,1,1,'ROLE_ADMIN',NULL ,1, '2017-12-17 18:46:19', 'loginoleg123@gmail.com',
+                            1,0,0,0,'Oleg', 'Login','$2a$11$uSXS6rLJ91WjgOHhEGDx..VGs7MkKZV68Lv5r1uwFu7HgtRn3dcXG','Travolta');
 
